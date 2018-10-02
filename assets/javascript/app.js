11e6b39e48309e7d1572449cf36b332f808d38d6
ericremoroza <eric.remoroza@outlook.com>
2018-10-02 19:40:05 -0400

    updated app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,13 @@ $(document).ready(function () {
     * @returns {string} URL for NYT API based on form inputs
     */
     function getQueryURL() {
-        var queryURL = "https://developers.zomato.com/api/v2.1/search";
-        var queryParams = {"user-key": "b62b830baae8f96987ca00269a09c856"};
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?q=bones";
+        //"https://developers.zomato.com/api/v2.1/search";
+        var queryParams = {"user-key": "fafaa7da981182588674fb22841c865f"};
         queryParams.q = $("#search-term").val().trim();
         console.log("URL :" + queryURL);
         console.log(queryURL + $.param(queryParams));
-        return queryURL + queryParams.p + $.param(queryParams);
+        return queryURL + queryParams.q; //+ queryParams.q + $.param(queryParams);
     }
 
     function updatePage(Data){
@@ -34,7 +35,11 @@ $(document).ready(function () {
         var queryURL = getQueryURL();
         $.ajax({
             url: queryURL,
-            method: "GET"
+            method: "GET",
+            beforeSend: (xhr) => {
+                xhr.setRequestHeader('Accept', 'application/json');
+                xhr.setRequestHeader('user-key', 'fafaa7da981182588674fb22841c865f');
+            }
         }).then(updatePage);
     });
     $("#clear-all").on("click", clear);
